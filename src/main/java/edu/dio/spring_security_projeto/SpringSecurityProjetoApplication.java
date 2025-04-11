1e68749d52187fbdea832ed4c75463782531228e
Vini <vini.ba.dev@gmail.com>
2025-04-11 09:28:19 -0300

    Init

---
+++ src/main/java/edu/dio/spring_security_projeto/SpringSecurityProjetoApplication.java
@@ -0,0 +1,13 @@
+package edu.dio.spring_security_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dio/spring_security_projeto/SpringSecurityProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dio.spring_security_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
